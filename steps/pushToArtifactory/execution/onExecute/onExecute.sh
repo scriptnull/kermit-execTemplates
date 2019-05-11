4698e0c9a52be49827843c4324f4e56a4d8bc8db
Ragesh Krishna <ragesh@rageshkrishna.com>
2019-05-11 09:28:58 +0530

    Update pushToArtifactory template

--- steps/pushToArtifactory/execution/onExecute/onExecute.sh
+++ steps/pushToArtifactory/execution/onExecute/onExecute.sh
@@ -1,5 +1,27 @@
 push_to_artifactory() {
-  echo "This is the pushToArtifactory native step"
+  local rtIntName="$artifactoryIntegrationName";
+  local rtIntMasterName=$(eval echo "$"res_"$rtIntName"_int_masterName)
+
+  if [ "$rtIntMasterName" != "artifactory" ]; then
+    echo "ERROR: $rtIntName is not an Artifactory integration"
+    exit 1;
+  fi
+
+  local rtUrl=$(eval echo "$"int_"$rtIntName"_url)
+  local rtUser=$(eval echo "$"int_"$rtIntName"_user)
+  local rtApiKey=$(eval echo "$"int_"$rtIntName"_apikey)
+
+  jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
+
+  if [ "$stepPackageType" == "files" ]; then
+    jfrog rt u $sourcePath $targetPath --build-name=$STEP_NAME --build-number=$STEP_ID
+    #jfrog rt bce $STEP_NAME $STEP_NUMBER
+    jfrog rt bp $STEP_NAME $STEP_NUMBER
+  elif [ "$stepPackageType" == "docker" ]; then
+    jfrog rt docker-push $imageTag $targetRepo --build-name=$STEP_NAME --build-number=$STEP_ID
+    #jfrog rt bce $STEP_NAME $STEP_ID
+    jfrog rt bp $STEP_NAME $STEP_ID
+  fi
 }
 
 execute_command push_to_artifactory
