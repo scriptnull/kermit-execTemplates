6b04f407148ed9651d57c93f4041a06cf0f3aa59
Ragesh Krishna <ragesh@rageshkrishna.com>
2019-05-11 13:45:15 +0530

    Add retry_command in pushToArtifactory

--- steps/pushToArtifactory/execution/onExecute/onExecute.sh
+++ steps/pushToArtifactory/execution/onExecute/onExecute.sh
@@ -11,16 +11,16 @@ push_to_artifactory() {
   local rtUser=$(eval echo "$"int_"$rtIntName"_user)
   local rtApiKey=$(eval echo "$"int_"$rtIntName"_apikey)
 
-  jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
+  retry_command jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
 
   if [ "$payloadType" == "file" ]; then
-    jfrog rt u $sourcePath $targetPath --build-name=$STEP_NAME --build-number=$STEP_ID
-    #jfrog rt bce $STEP_NAME $STEP_ID
-    jfrog rt bp $STEP_NAME $STEP_ID
+    retry_command jfrog rt u $sourcePath $targetPath --build-name=$STEP_NAME --build-number=$STEP_ID
+    #retry_command jfrog rt bce $STEP_NAME $STEP_ID
+    retry_command jfrog rt bp $STEP_NAME $STEP_ID
   elif [ "$payloadType" == "docker" ]; then
-    jfrog rt docker-push $imageTag $targetRepo --build-name=$STEP_NAME --build-number=$STEP_ID
-    #jfrog rt bce $STEP_NAME $STEP_ID
-    jfrog rt bp $STEP_NAME $STEP_ID
+    retry_command jfrog rt docker-push $imageTag $targetRepo --build-name=$STEP_NAME --build-number=$STEP_ID
+    #retry_command jfrog rt bce $STEP_NAME $STEP_ID
+    retry_command jfrog rt bp $STEP_NAME $STEP_ID
   else
     echo "Unknown packageType: $payloadType"
     exit 1
