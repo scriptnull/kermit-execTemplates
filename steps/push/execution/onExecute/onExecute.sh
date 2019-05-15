5a33dd2fd35311cc6c2089aa0d5754448019329c
Ragesh Krishna <ragesh@rageshkrishna.com>
2019-05-15 16:35:17 +0530

    Update push template to work with new docker push specification

--- steps/push/execution/onExecute/onExecute.sh
+++ steps/push/execution/onExecute/onExecute.sh
@@ -1,26 +1,35 @@
 push_to_artifactory() {
-  local rtIntName="$artifactoryIntegrationName";
-  local rtIntMasterName=$(eval echo "$"int_"$rtIntName"_masterName)
+  local outputIntName="$outputIntegrationName";
+  local outputIntMasterName=$(eval echo "$"int_"$outputIntName"_masterName)
 
-  if [ "$rtIntMasterName" != "artifactory" ]; then
-    echo "ERROR: $rtIntName is not an Artifactory integration"
+  if [ "$outputIntMasterName" == "artifactory" ]; then
+    local rtUrl=$(eval echo "$"int_"$rtIntName"_url)
+    local rtUser=$(eval echo "$"int_"$rtIntName"_user)
+    local rtApiKey=$(eval echo "$"int_"$rtIntName"_apikey)
+    retry_command jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
+  elif [ "$outputIntMasterName" == "dockerRegistryLogin" ]; then
+    local dhUrl=$(eval echo "$"int_"$rtIntName"_url)
+    local dhUsername=$(eval echo "$"int_"$rtIntName"_username)
+    local dhPassword=$(eval echo "$"int_"$rtIntName"_password)
+    retry_command docker login -u "$userName" -p "$password" "$url"
+  else
+    echo "$outputIntegrationName is not an Artifactory or Docker Registry integration"
     exit 1
   fi
-
-  local rtUrl=$(eval echo "$"int_"$rtIntName"_url)
-  local rtUser=$(eval echo "$"int_"$rtIntName"_user)
-  local rtApiKey=$(eval echo "$"int_"$rtIntName"_apikey)
-
-  retry_command jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
 
   if [ "$payloadType" == "file" ]; then
     retry_command jfrog rt u $sourcePath $targetPath --build-name=$STEP_NAME --build-number=$STEP_ID
     #retry_command jfrog rt bce $STEP_NAME $STEP_ID
     retry_command jfrog rt bp $STEP_NAME $STEP_ID
   elif [ "$payloadType" == "docker" ]; then
-    retry_command jfrog rt docker-push $imageTag $targetRepo --build-name=$STEP_NAME --build-number=$STEP_ID
-    #retry_command jfrog rt bce $STEP_NAME $STEP_ID
-    retry_command jfrog rt bp $STEP_NAME $STEP_ID
+    docker tag $inputImageName:$inputImageTag $outputImageName:$outputImageTag
+    if [ "$outputIntMasterName" == "artifactory" ]; then
+      retry_command jfrog rt docker-push $outputImageName:$outputImageTag $targetRepo --build-name=$STEP_NAME --build-number=$STEP_ID
+      #retry_command jfrog rt bce $STEP_NAME $STEP_ID
+      retry_command jfrog rt bp $STEP_NAME $STEP_ID
+    elif [ "$outputIntMasterName" == "dockerRegistryLogin" ]; then
+      retry_command docker push $outputImageName:$outputImageTag
+    fi
   else
     echo "Unknown packageType: $payloadType"
     exit 1
