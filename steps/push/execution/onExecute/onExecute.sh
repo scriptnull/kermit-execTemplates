788f58507fb9c20e1136004e6faffab82712f608
Ragesh Krisna <ragesh@rageshkrishna.com>
2019-05-15 11:35:23 +0000

    Fix env names for push step

--- steps/push/execution/onExecute/onExecute.sh
+++ steps/push/execution/onExecute/onExecute.sh
@@ -3,25 +3,25 @@ push_to_artifactory() {
   local outputIntMasterName=$(eval echo "$"int_"$outputIntName"_masterName)
 
   if [ "$outputIntMasterName" == "artifactory" ]; then
-    local rtUrl=$(eval echo "$"int_"$rtIntName"_url)
-    local rtUser=$(eval echo "$"int_"$rtIntName"_user)
-    local rtApiKey=$(eval echo "$"int_"$rtIntName"_apikey)
+    local rtUrl=$(eval echo "$"int_"$outputIntName"_url)
+    local rtUser=$(eval echo "$"int_"$outputIntName"_user)
+    local rtApiKey=$(eval echo "$"int_"$outputIntName"_apikey)
     retry_command jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
   elif [ "$outputIntMasterName" == "dockerRegistryLogin" ]; then
-    local dhUrl=$(eval echo "$"int_"$rtIntName"_url)
-    local dhUsername=$(eval echo "$"int_"$rtIntName"_username)
-    local dhPassword=$(eval echo "$"int_"$rtIntName"_password)
+    local dhUrl=$(eval echo "$"int_"$outputIntName"_url)
+    local dhUsername=$(eval echo "$"int_"$outputIntName"_username)
+    local dhPassword=$(eval echo "$"int_"$outputIntName"_password)
     retry_command docker login -u "$userName" -p "$password" "$url"
   else
     echo "$outputIntegrationName is not an Artifactory or Docker Registry integration"
     exit 1
   fi
 
-  if [ "$payloadType" == "file" ]; then
+  if [ "$step_payloadType" == "file" ]; then
     retry_command jfrog rt u $sourcePath $targetPath --build-name=$STEP_NAME --build-number=$STEP_ID
     #retry_command jfrog rt bce $STEP_NAME $STEP_ID
     retry_command jfrog rt bp $STEP_NAME $STEP_ID
-  elif [ "$payloadType" == "docker" ]; then
+  elif [ "$step_payloadType" == "docker" ]; then
     docker tag $inputImageName:$inputImageTag $outputImageName:$outputImageTag
     if [ "$outputIntMasterName" == "artifactory" ]; then
       retry_command jfrog rt docker-push $outputImageName:$outputImageTag $targetRepo --build-name=$STEP_NAME --build-number=$STEP_ID
