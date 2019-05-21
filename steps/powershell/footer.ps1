ff1cf326fcda7948a55d7d66a2dfc0d77bc48731
Vishnu Bharathi <vishnubharathi04@gmail.com>
2019-05-21 18:56:12 +0530

    Adds footer for powershell step

---
+++ steps/powershell/footer.ps1
@@ -0,0 +1,25 @@
+Function before_exit() {
+  if ($global:is_success) {
+    Write-Output "__SH__SCRIPT_END_SUCCESS__"
+  } else {
+    Write-Output "__SH__SCRIPT_END_FAILURE__"
+  }
+}
+
+Function main() {
+  $global:is_success = $TRUE
+  Try
+  {
+    onExecute
+  }
+  Catch
+  {
+    $global:is_success = $FALSE
+  }
+  Finally
+  {
+    before_exit
+  }
+}
+
+main
