b3136f1d6f971e7f68b2fda1b6466a0b3d0878f5
Ragesh Krishna <ragesh@rageshkrishna.com>
2019-05-21 23:32:22 +0000

    Enhance promote template

--- steps/promote/execution/onExecute/onExecute.sh
+++ steps/promote/execution/onExecute/onExecute.sh
@@ -1,13 +1,16 @@
 promote() {
+  echo "[promote] Authenticating with integration: $artifactoryIntegrationName"
   local rtUrl=$(eval echo "$"int_"$artifactoryIntegrationName"_url)
   local rtUser=$(eval echo "$"int_"$artifactoryIntegrationName"_user)
   local rtApiKey=$(eval echo "$"int_"$artifactoryIntegrationName"_apikey)
   retry_command jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
 
-  if [ ! -z "$inputBuildInfoResourceName" ]; then
-    echo "[promote] Using build name and number from buildInfo resource $inputBuildInfoResourceName"
-    buildName=$(eval echo "$"res_"$inputBuildInfoResourceName"_buildName)
-    buildNumber=$(eval echo "$"res_"$inputBuildInfoResourceName"_buildNumber)
+  if [ -z "$buildName " ] && [ -z "$buildNumber" ]; then
+    if [ ! -z "$inputBuildInfoResourceName" ]; then
+      echo "[promote] Using build name and number from buildInfo resource: $inputBuildInfoResourceName"
+      buildName=$(eval echo "$"res_"$inputBuildInfoResourceName"_buildName)
+      buildNumber=$(eval echo "$"res_"$inputBuildInfoResourceName"_buildNumber)
+    fi
   fi
 
   echo "[promote] Promoting build $buildName/$buildNumber to $targetRepo"
