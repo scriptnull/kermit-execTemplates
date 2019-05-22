46e9b17110a635da8d89b8b68c237d69bd6f6e8a
Ragesh Krisna <ragesh@rageshkrishna.com>
2019-05-22 15:54:26 +0000

    Fix bad zero check in promote script

--- steps/promote/execution/onExecute/onExecute.sh
+++ steps/promote/execution/onExecute/onExecute.sh
@@ -5,7 +5,7 @@ promote() {
   local rtApiKey=$(eval echo "$"int_"$artifactoryIntegrationName"_apikey)
   retry_command jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
 
-  if [ -z "$buildName " ] && [ -z "$buildNumber" ]; then
+  if [ -z "$buildName" ] && [ -z "$buildNumber" ]; then
     if [ ! -z "$inputBuildInfoResourceName" ]; then
       echo "[promote] Using build name and number from buildInfo resource: $inputBuildInfoResourceName"
       buildName=$(eval echo "$"res_"$inputBuildInfoResourceName"_buildName)
@@ -13,11 +13,11 @@ promote() {
     fi
   fi
 
-  echo "[promote] Promoting build $buildName/$buildNumber to $targetRepo"
+  echo "[promote] Promoting build $buildName/$buildNumber to: $targetRepo"
   retry_command jfrog rt build-promote $buildName $buildNumber $targetRepo
 
   if [ ! -z "$outputPromoteInfoResourceName" ]; then
-    echo "[promote] Updating output resource $outputPromoteInfoResourceName"
+    echo "[promote] Updating output resource: $outputPromoteInfoResourceName"
     write_output $outputPromoteInfoResourceName buildName=$buildName buildNumber=$buildNumber targetRepo=$targetRepo
   fi
 }
