461c4a5788a8f6ab2e86f6c4abdf85f80f1f30f8
ankul <ankul@shippable.com>
2019-06-08 16:43:08 +0530

    pr comment

--- steps/NpmPublish/execution/onExecute/onExecute.sh
+++ steps/NpmPublish/execution/onExecute/onExecute.sh
@@ -6,15 +6,16 @@ publish() {
   retry_command jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
 
   local sourceStateName=$(eval echo "$""$inputNpmBuildStepName"_sourceStateName)
-  restore_run_state $sourceStateName /tmp/jfrog/$sourceStateName
+  local tempStateLocation="$step_tmp_dir/npmSourceState"
+  restore_run_state $sourceStateName $tempStateLocation
 
   local buildNumber=$(eval echo "$""$inputNpmBuildStepName"_buildNumber)
   local buildName=$(eval echo "$""$inputNpmBuildStepName"_buildName)
   local sourceLocation=$(eval echo "$""$inputNpmBuildStepName"_sourceLocation)
   local repositoryName=$(jq -r ".step.configuration.repositoryName" $step_json_path)
 
-  echo -e "\n[NpmPublish] Changing directory: /tmp/jfrog/$sourceStateName/$sourceLocation"
-  pushd /tmp/jfrog/$sourceStateName/$sourceLocation
+  pushd $tempStateLocation/$sourceLocation
+    echo -e "\n[NpmPublish] Publishing npm packages to repository: $repositoryName"
     jfrog rt npm-publish $repositoryName --build-name=$buildName --build-number=$buildNumber
   popd
 
