ff1e78f16bc0d4d2752c97bae2922e86c5de5b67
Deepika Banoth <deepikabanoth@deepikab-mac.local>
2019-06-08 10:27:48 +0530

    rename promote -> PromoteBuild

--- steps/PromoteBuild/execution/onExecute/onExecute.sh
+++ steps/PromoteBuild/execution/onExecute/onExecute.sh
@@ -1,5 +1,5 @@
-promote() {
-  echo "[promote] Authenticating with integration: $artifactoryIntegrationName"
+PromoteBuild() {
+  echo "[PromoteBuild] Authenticating with integration: $artifactoryIntegrationName"
   local rtUrl=$(eval echo "$"int_"$artifactoryIntegrationName"_url)
   local rtUser=$(eval echo "$"int_"$artifactoryIntegrationName"_user)
   local rtApiKey=$(eval echo "$"int_"$artifactoryIntegrationName"_apikey)
@@ -7,17 +7,17 @@ promote() {
 
   if [ -z "$buildName" ] && [ -z "$buildNumber" ]; then
     if [ ! -z "$inputBuildInfoResourceName" ]; then
-      echo "[promote] Using build name and number from BuildInfo resource: $inputBuildInfoResourceName"
+      echo "[PromoteBuild] Using build name and number from BuildInfo resource: $inputBuildInfoResourceName"
       buildName=$(eval echo "$"res_"$inputBuildInfoResourceName"_buildName)
       buildNumber=$(eval echo "$"res_"$inputBuildInfoResourceName"_buildNumber)
     elif [ ! -z "$buildStepName" ]; then
       buildName=$(eval echo "$""$buildStepName"_buildName)
       buildNumber=$(eval echo "$""$buildStepName"_buildNumber)
     else
-      echo "[promote] ERROR: Unable to find a build name and number to work with."
-      echo "[promote] Please use environment variables, an input BuildInfo resource"
-      echo "[promote] or configure promote to run in an affinity group that also"
-      echo "[promote] runs a build step."
+      echo "[PromoteBuild] ERROR: Unable to find a build name and number to work with."
+      echo "[PromoteBuild] Please use environment variables, an input BuildInfo resource"
+      echo "[PromoteBuild] or configure PromoteBuild to run in an affinity group that also"
+      echo "[PromoteBuild] runs a build step."
       exit 1;
     fi
   fi
@@ -25,19 +25,19 @@ promote() {
   targetRepo=$(jq -r ".step.setup.promote.targetRepo" $step_json_path)
   includeDependencies=$(jq -r ".step.setup.promote.includeDependencies" $step_json_path)
 
-  echo "[promote] Promoting build $buildName/$buildNumber to: $targetRepo"
-  local promoteCmd="jfrog rt build-promote $buildName $buildNumber $targetRepo"
+  echo "[PromoteBuild] Promoting build $buildName/$buildNumber to: $targetRepo"
+  local PromoteBuildCmd="jfrog rt build-promote $buildName $buildNumber $targetRepo"
   if [ ! -z "$includeDependencies" ] && [ "$includeDependencies" == "true" ]; then
-    promoteCmd="$promoteCmd --include-dependencies"
-    echo "[promote] (including dependencies)"
+    PromoteBuildCmd="$PromoteBuildCmd --include-dependencies"
+    echo "[PromoteBuild] (including dependencies)"
   fi
 
-  retry_command $promoteCmd
+  retry_command $PromoteBuildCmd
 
   if [ ! -z "$outputBuildInfoResourceName" ]; then
-    echo "[promote] Updating output resource: $outputBuildInfoResourceName"
+    echo "[PromoteBuild] Updating output resource: $outputBuildInfoResourceName"
     write_output $outputBuildInfoResourceName buildName=$buildName buildNumber=$buildNumber targetRepo=$targetRepo
   fi
 }
 
-execute_command promote
+execute_command PromoteBuild
