87bedf243bfb689b57c49dd6e025ea204ff8bd22
Ragesh Krishna <ragesh@rageshkrishna.com>
2019-06-08 05:17:09 +0000

    Update JSON schema and template for DockerBuild

--- steps/DockerBuild/execution/onExecute/onExecute.sh
+++ steps/DockerBuild/execution/onExecute/onExecute.sh
@@ -1,51 +1,45 @@
-build() {
-  echo "[build] Authenticating with integration: $artifactoryIntegrationName"
+DockerBuild() {
+  echo "[DockerBuild] Authenticating with integration: $artifactoryIntegrationName"
   local rtUrl=$(eval echo "$"int_"$artifactoryIntegrationName"_url)
   local rtUser=$(eval echo "$"int_"$artifactoryIntegrationName"_user)
   local rtApiKey=$(eval echo "$"int_"$artifactoryIntegrationName"_apikey)
   retry_command jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
 
-  payloadType=$step_payloadType
   buildName=$pipeline_name
   buildNumber=$run_number
 
   buildDir=$(eval echo "$"res_"$inputGitRepoResourceName"_resourcePath)/$dockerFileLocation
-  echo "[build] Changing directory: $buildDir"
+  echo "[DockerBuild] Changing directory: $buildDir"
   pushd $buildDir
     if [ ! -z "$inputFileResourceName" ]; then
       filePath=$(eval echo "$"res_"$inputFileResourceName"_resourcePath)/*
-      echo "[build] Copying files from: $filePath to: $(pwd)"
+      echo "[DockerBuild] Copying files from: $filePath to: $(pwd)"
       # todo: remove -v
       cp -vr $filePath .
     fi
 
-    if [ "$payloadType" == "docker" ]; then
-      dockerFileLocation=$(jq -r ".step.setup.build.dockerFileLocation" $step_json_path)
-      dockerFileName=$(jq -r ".step.setup.build.dockerFileName" $step_json_path)
-      imageName=$(jq -r ".step.setup.build.imageName" $step_json_path)
-      imageTag=$(jq -r ".step.setup.build.imageTag" $step_json_path)
-      evalImageName=$(eval echo $imageName)
-      evalImageTag=$(eval echo $imageTag)
-      echo "[build] Building docker image: $evalImageName:$evalImageTag using Dockerfile: ${dockerFileName}"
-      docker build -t $evalImageName:$evalImageTag -f ${buildDir}/${dockerFileName} .
+    dockerFileLocation=$(jq -r ".step.configuration.dockerFileLocation" $step_json_path)
+    dockerFileName=$(jq -r ".step.configuration.dockerFileName" $step_json_path)
+    dockerImageName=$(jq -r ".step.configuration.dockerImageName" $step_json_path)
+    dockerImageTag=$(jq -r ".step.configuration.dockerImageTag" $step_json_path)
+    evalDockerImageName=$(eval echo $dockerImageName)
+    evalDockerImageTag=$(eval echo $dockerImageTag)
+    echo "[DockerBuild] Building docker image: $evalDockerImageName:$evalDockerImageTag using Dockerfile: ${dockerFileName}"
+    docker build -t $evalDockerImageName:$evalDockerImageTag -f ${buildDir}/${dockerFileName} .
 
-      echo "[build] Adding build information to pipeline state"
-      add_pipeline_variable buildStepName=${step_name}
-      add_pipeline_variable ${step_name}_payloadType=${payloadType}
-      add_pipeline_variable ${step_name}_buildNumber=${buildNumber}
-      add_pipeline_variable ${step_name}_buildName=${buildName}
-      add_pipeline_variable ${step_name}_isPromoted=false
-      add_pipeline_variable ${step_name}_imageName=${evalImageName}
-      add_pipeline_variable ${step_name}_imageTag=${evalImageTag}
+    echo "[DockerBuild] Adding build information to pipeline state"
+    add_run_variable buildStepName=${step_name}
+    add_run_variable ${step_name}_payloadType="docker"
+    add_run_variable ${step_name}_buildNumber=${buildNumber}
+    add_run_variable ${step_name}_buildName=${buildName}
+    add_run_variable ${step_name}_isPromoted=false
+    add_run_variable ${step_name}_dockerImageName=${evalDockerImageName}
+    add_run_variable ${step_name}_dockerImageTag=${evalDockerImageTag}
 
-    else
-      echo "[build] Unsupported payloadType: $payloadType"
-      exit 1
-    fi
   popd
 
   jfrog rt bce $buildName $buildNumber
   save_run_state /tmp/jfrog/. jfrog
 }
 
-execute_command build
+execute_command DockerBuild
