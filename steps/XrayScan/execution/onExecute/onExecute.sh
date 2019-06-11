a14d92f421ffe60e2a81a7377fc910dae1e8eae2
Aidan Murphy <aidanm@jfrog.com>
2019-06-11 16:08:08 -0700

    #391; updates XrayScan.

--- steps/XrayScan/execution/onExecute/onExecute.sh
+++ steps/XrayScan/execution/onExecute/onExecute.sh
@@ -1,38 +1,36 @@
 scan() {
-  echo "[scan] Authenticating with integration: $artifactoryIntegrationName"
-  local rtUrl=$(eval echo "$"int_"$artifactoryIntegrationName"_url)
-  local rtUser=$(eval echo "$"int_"$artifactoryIntegrationName"_user)
-  local rtApiKey=$(eval echo "$"int_"$artifactoryIntegrationName"_apikey)
+  local integrationAlias=$(eval echo "$"res_"$inputBuildInfoResourceName"_integrationAlias)
+  local integrationName=$(eval echo "$"res_"$inputBuildInfoResourceName"_"$integrationAlias"_name)
+
+  echo "[scan] Authenticating with integration: $integrationName"
+  local rtUrl=$(eval echo "$"res_"$inputBuildInfoResourceName"_"$integrationAlias"_url)
+  local rtUser=$(eval echo "$"res_"$inputBuildInfoResourceName"_"$integrationAlias"_user)
+  local rtApiKey=$(eval echo "$"res_"$inputBuildInfoResourceName"_"$integrationAlias"_apikey)
+
   retry_command jfrog rt config --url $rtUrl --user $rtUser --apikey $rtApiKey --interactive=false
 
   local buildName=""
   local buildNumber=""
-  local stepConfiguration=$(cat $step_json_path | jq .step.configuration)
   if [ ! -z "$inputBuildInfoResourceName" ]; then
     echo "[scan] Using build name and number from BuildInfo resource: $inputBuildInfoResourceName"
     buildName=$(eval echo "$"res_"$inputBuildInfoResourceName"_buildName)
     buildNumber=$(eval echo "$"res_"$inputBuildInfoResourceName"_buildNumber)
-  elif [ ! -z "$buildStepName" ]; then
-    buildName=$(eval echo "$""$buildStepName"_buildName)
-    buildNumber=$(eval echo "$""$buildStepName"_buildNumber)
-  elif [ ! -z "$stepConfiguration" ] && [ "$stepConfiguration" != "null" ]; then
-    # TODO: fix this when setup section gets exported as envs
-    buildNameVar=$(echo $stepConfiguration | jq -r .buildName)
-    buildNumberVar=$(echo $stepConfiguration | jq -r .buildNumber)
-    buildName=$(eval echo $buildNameVar)
-    buildNumber=$(eval echo $buildNumberVar)
   fi
 
   if [ -z "$buildName" ] && [ -z "$buildNumber" ]; then
     echo "[scan] ERROR: Unable to find a build name and number to work with."
-    echo "[scan] Please use environment variables, an input BuildInfo resource"
-    echo "[scan] or configure scan to run in an affinity group that also"
-    echo "[scan] runs a build step."
+    echo "[scan] Please use an input BuildInfo resource."
     exit 1;
   fi
 
   echo "[scan] Scanning build $buildName/$buildNumber"
   jfrog rt bs $buildName $buildNumber
+
+  if [ ! -z "$outputBuildInfoResourceName" ]; then
+    echo -e "\n[scan] Updating output resource: $outputBuildInfoResourceName"
+
+    replicate_resource $inputBuildInfoResourceName $outputBuildInfoResourceName
+  fi
 }
 
 execute_command scan
