46692f74738908c74b5af11eea7092155e446ea4
Bharath Upadhya <1992bharath@gmail.com>
2019-06-18 12:18:40 +0530

    removes unused scripts

--- WindowsServer_2016/job/build.ps1
+++
@@ -1,6 +0,0 @@
-Function main([string] $scriptPath, [string] $envsPath) {
-  <%= obj.reqExecCommand %> "$scriptPath" "$envsPath"
-  exit $LASTEXITCODE
-}
-
-main @args
--- WindowsServer_2016/job/replace_placeholders.ps1
+++
@@ -1,8 +0,0 @@
-Function replace_placeholders() {
-    $versionPath = "<%= obj.versionPath %>"
-    if (Test-Path "$versionPath") {
-        exec_cmd "shippable_replace $versionPath"
-    }
-}
-
-replace_placeholders
